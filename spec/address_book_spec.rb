dd87c9fac09151b4159ded2c8e98aa1dc87a3660
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2015-09-12 15:49:48 -0400

    checkpoint-21 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,53 +1,32 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
+
+  let(:book) {AddressBook.new}
+
+  def check_entry(entry, expected_name, expected_number, expected_email)
+    expect(entry.name).to eql expected_name
+    expect(entry.phone_number).to eql expected_number
+    expect(entry.email).to eql expected_email
+  end
+
   describe "attributes" do
     it "should respond to entries" do
-      book = AddressBook.new
       expect(book).to respond_to(:entries)
     end
     it "should initialize entires as an array" do
-      book = AddressBook.new
       expect(book.entries).to be_a(Array)
     end
     it "should initialize entries as empty" do
-      book = AddressBook.new
       expect(book.entries.size).to eq(0)
     end
   end
   describe "#add_entry" do
     it "adds only one entry to the address book" do
-      book = AddressBook.new
       require_relative '../models/entry'
-
-      RSpec.describe Entry do
-        describe "attributes" do
-          it "should respond to name" do
-            entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-            expect(entry).to respond_to(:name)
-          end
-          it "should respond to phone number" do
-            entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-            expect(entry).to respond_to(:phone_number)
-          end
-          it "should respond to email" do
-            entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-            expect(entry).to respond_to(:email)
-          end
-        end
-        describe "#to_s" do
-          it "prints an entry as a string" do
-            entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-            expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
-            expect(entry.to_s).to eq(expected_string)
-          end
-        end
-      end
-
-      expect(book.entries.size).to eq(1)
     end
+
     it "adds the correct information to entries" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       new_entry = book.entries[0]
 
@@ -57,4 +36,51 @@ RSpec.describe AddressBook do
 
     end
   end
+
+  #Test that AddressBook's .import_from_csv() method is working as expected.
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries.csv")
+      book_size = book.entries.size
+
+      #Check the size of the entries in AddressBook
+      expect(book_size).to eql 5
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3nd entry" do
+      book.import_from_csv("entries.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+    end
+
+    it "imports the 4th entry" do
+      book.import_from_csv("entries.csv")
+      entry_four = book.entries[3]
+      check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+    end
+
+    it "imports the 5th entry" do
+      book.import_from_csv("entries.csv")
+      entry_five = book.entries[4]
+      check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+    end
+
+  end
+
+
+
+
+
 end
