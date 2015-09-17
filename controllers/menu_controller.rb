f40f5c244899af74ed14406d3f1f76c376917039
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2015-09-16 22:34:11 -0400

    checkpoint-23 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -86,6 +86,18 @@ class MenuController
   end
 
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+
+    match = address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
   def read_csv
@@ -137,6 +149,30 @@ class MenuController
     puts "Updated entry:"
     puts entry
 
+  def search_submenu(entry)
+
+     puts "\nd - delete entry"
+     puts "e - edit this entry"
+     puts "m - return to main menu"
+
+     selection = gets.chomp
+
+     case selection
+     when "d"
+       system "clear"
+       delete_entry(entry)
+       main_menu
+     when "e"
+       system "clear"
+       main_menu
+     else
+       system "clear"
+       puts "#{selection} is not a valid input"
+       puts entry.to_s
+       search_submenu(entry)
+     end
+   end
+   
 
 
 
@@ -152,8 +188,11 @@ class MenuController
     when "n"
 
     when "d"
+      delete_entry(entry)
 
     when "e"
+      edit_entry(entry)
+      entry_submenu(entry)
 
     when "m"
       system "clear"
